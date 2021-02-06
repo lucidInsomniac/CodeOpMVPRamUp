aa08ab1d2e3d6e670828bb2a8ea1316f1200204c
donnchin <donnchin@gmail.com>
2021-02-06 01:13:30 -0500

    added and commented out the  actual code for Recv and Inventory rendering with conditions.

--- client/src/components/UserComponents/Inventory/Inventory.js
+++ client/src/components/UserComponents/Inventory/Inventory.js
@@ -6,6 +6,62 @@ export default function Inventory (props) {
     return (
         <div className="Inventory">
             <h2>Inventory</h2>
+
+
+{/***********This page will render all data from MYSQL database****************
+ * 
+ *           Condition: GET ALL Only if Full Order = Yes
+ * 
+ *          
+ * 
+                    <table class="pure-table">
+                        {props.inventory && 
+                            props.inventory.map =(i => (
+                            
+                                <thead>
+                                    <tr>
+                                        <th>Order Date</th>
+                                        <th>Vendor</th>
+                                        <th>Team</th>
+                                        <th>Item</th>
+                                        <th>Size</th>
+                                        <th>QTY</th>
+                                        <th>Partial Order</th>
+                                        <th>Full Order</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr 
+                                        key={ i.id }
+                                        class="pure-table-odd">
+                                            <td>{ i.ordDate }</td>
+                                            <td>{ i.vendor }</td>
+                                            <td>{ i.team }</td>
+                                            <td>{ i.item }</td>
+                                            <td>{ i.size }</td>
+                                            <td>{ i.qty }</td>
+                                            <td>{ i.partOrd }</td>
+                                            <td{ i.fullOrd }</td>
+                                    </tr>
+                                </tbody>
+                    </table>
+                ))}
+  
+    
+ *              
+ *              
+ *                
+ *              
+ * 
+*******************************************************************************
+
+
+
+
+
+
+{/***********************For MOCKUP DISPLAY ONLY****************************** */}
+
             <div className="table container"> 
             <table class="pure-table">
                 <thead>
--- client/src/components/UserComponents/Recv/Recv.js
+++ client/src/components/UserComponents/Recv/Recv.js
@@ -6,6 +6,75 @@ export default function Recv (props) {
       <div className="Recv "> 
         <h2>Current Team Orders</h2> 
         {/* list items from form here */}
+
+
+
+{/***********This page will render all data from MYSQL database****************
+ * 
+ *           Condition: GET ALL Only if Partial = Yes or empty
+ * 
+ *          
+ * 
+                    <table class="pure-table">
+                        {props.inventory && 
+                            props.inventory.map =(i => (
+                            
+                                <thead>
+                                    <tr>
+                                        <th>Order Date</th>
+                                        <th>Vendor</th>
+                                        <th>Team</th>
+                                        <th>Item</th>
+                                        <th>Size</th>
+                                        <th>QTY</th>
+                                        <th>Partial Order</th>
+                                        <th>Full Order</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr 
+                                        key={ i.id }
+                                        class="pure-table-odd">
+                                            <td>{ i.ordDate }</td>
+                                            <td>{ i.vendor }</td>
+                                            <td>{ i.team }</td>
+                                            <td>{ i.item }</td>
+                                            <td>{ i.size }</td>
+                                            <td>{ i.qty }</td>
+                                            <td>
+                                                <select>
+                                                    <option>Select</option>
+                                                    <option>Yes</option>
+                                                    <option>No</option>
+                                                </select>
+                                            </td>
+                                            <td>
+                                                <select>
+                                                    <option>Select</option>
+                                                    <option>Yes</option>
+                                                    <option>No</option>
+                                                </select>
+                                            </td>
+                                    </tr>
+                                </tbody>
+                    </table>
+                ))}
+  
+    
+ *              
+ *              
+ *                
+ *              
+ * 
+*******************************************************************************
+
+
+
+
+
+
+{/***********************For MOCKUP DISPLAY ONLY****************************** */}
+
         <div className="table container"> 
           <table className="pure-table">
               <thead>
